7448c3e9e03733b7eb6b5dc01a857ede70855353
wassim triki <wsmtriki@gmail.com>
2022-10-26 14:42:54 +0100

    fix layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,15 +12,19 @@ const Card = ({ id, denomination, stars_count }) => {
   }
 
   return (
-    <div className="  rounded-sm shadow-md">
+    <div className="  rounded-sm shadow-md group w-[300px] h-[300px]">
       <div className="bg-blue-200 h-[200px] overflow-hidden">
+        {/* <div className="h-full w-full bg-black bg-opacity-50 hidden group-hover:block  first-letter text-white p-4">
+          <h2 className="font-bold">Specialty :</h2>
+        </div> */}
+        {/* <div className="bg-blue-500 bg-opacity-50 h-full w-full "></div> */}
         <img
           className="w-full h-full object-cover"
           src={profilePictureURL}
           alt=""
         />
       </div>
-      <div className="flex flex-col p-2 gap-4">
+      <div className="flex flex-col p-2 gap-4 ">
         <h2 className="self-center font-extrabold text-xl">{denomination}</h2>
         <div className="self-end flex items-center gap-1">
           {stars_count} <AiFillStar className="text-xl text-yellow-400" />
